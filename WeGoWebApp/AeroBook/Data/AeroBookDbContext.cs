17163d3d4e766bb2fdd05fa2be826a500600a635
C9tech <d9098111@gmail.com>
2023-09-08 17:48:14 +0530

    Created viewmodel and datamodels seperatley

--- WeGoWebApp/AeroBook/Data/AeroBookDbContext.cs
+++ WeGoWebApp/AeroBook/Data/AeroBookDbContext.cs
@@ -1,13 +1,13 @@
 ﻿using AeroBook.Models.Account;
 using Microsoft.EntityFrameworkCore;
-using AeroBook.Models.Flights;
+using AeroBook.Data.Models.Account;
 
 namespace AeroBook.Data
 {
-	public class AeroBookDbContext: DbContext
+    public class AeroBookDbContext: DbContext
 	{
 		public AeroBookDbContext(DbContextOptions options) : base(options) { }
 		public DbSet<User> Users { get; set; }
-		public DbSet<AeroBook.Models.Flights.Flightdetails> Flightdetails { get; set; } = default!;
+		public DbSet<Models.Flights.Flightdetails> Flightdetails { get; set; } = default!;
 	}
 }
